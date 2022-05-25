274ee38856a36616c4305dbf31c8de09d1b8133c
Marina Kovaleva <marinakovaleva259@gmail.com>
2022-05-25 22:19:20 +0300

    Implement elements and move for players

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function createField() {
+  var field = document.createElement("div");
+  field.className = "field";
+  var container = document.querySelector(".container");
+  container.prepend(field);
+}
+createField();
+
+function createBtn(count) {
+  var btn = document.createElement("button");
+  btn.className = "btn";
+  btn.setAttribute("id", count);
+  document.querySelector(".field").append(btn);
+}
+
+function generateField(number) {
+  var countBtn = number;
+  for (var i = 0; i < countBtn; i++) {
+    createBtn(i + 1);
+  }
+}
+generateField(9);
+
+var buttons = document.querySelectorAll(".btn");
+var sign = 0;
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    sign++;
+    var elem = e.target;
+    elem.innerHTML = sign % 2 === 0 ? "X" : "0";
+  });
+});
+
+// console.log(field);
